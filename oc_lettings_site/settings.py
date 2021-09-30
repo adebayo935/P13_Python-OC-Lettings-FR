710b1a5579224e06ef564bd430638e52e6366ff4
RenoFar <renaudfaraci@gmail.com>
2021-09-30 19:33:10 +0200

    updating root staticfiles

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -112,11 +112,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',
+    os.path.join(BASE_DIR, 'static'),
 ]
 
 
