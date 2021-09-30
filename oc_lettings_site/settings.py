48bcc3e0147f417e644e854ccf0a53b0550c9ad5
RenoFar <renaudfaraci@gmail.com>
2021-09-30 15:36:56 +0200

    updating template & css

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -22,14 +22,14 @@ ALLOWED_HOSTS = ['oc-lettings-p13.herokuapp.com', '0.0.0.0', '127.0.0.1', 'local
 
 INSTALLED_APPS = [
     'oc_lettings_site.apps.OCLettingsSiteConfig',
+    'lettings.apps.LettingsConfig',
+    'profiles.apps.ProfilesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'lettings',
-    'profiles',
 ]
 
 MIDDLEWARE = [
@@ -112,12 +112,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Extra places for collectstatic to find static files.
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'static'),
-# ]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 
 # Simplified static file serving.
